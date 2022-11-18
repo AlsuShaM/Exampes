7e96d2e29a38e1633c06dcf2fc0ce2695756dd52
Alsu <shakilsu@yandex.ru>
2022-11-19 02:22:34 +0300

    Examples03

---
+++ Examples03/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); //1234...9
+Console.WriteLine (numberA);
+int numberВ = new Random().Next(1,10);
+Console.WriteLine (numberВ);
+int result = numberA + numberВ;
+Console.WriteLine(result);
