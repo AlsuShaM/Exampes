fe6159a45721eafa7c36f513f85432d0fe3828a3
Alsu <shakilsu@yandex.ru>
2022-11-19 12:25:22 +0300

    Examples05

---
+++ Examples05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алсу")
+{
+    Console.WriteLine("Ура, это же Алсу");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
