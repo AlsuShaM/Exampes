af742fe568c97bfafc69b6de7f69470e3f17877e
Alsu <shakilsu@yandex.ru>
2022-11-21 16:27:36 +0300

    8910

---
+++ Example10/Program.cs
@@ -0,0 +1,16 @@
+﻿// имеется массив array и n элементов, требуется найти элемент массива равный find
+
+int[] array = {5, 1, 8, 2, 33, 7, 2, 9};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
